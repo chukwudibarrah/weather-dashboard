958aaaa1d3d558a3dbf85916d0feeb7b86af17e5
chukwudibarrah <ceebrrh@gmail.com>
2023-02-05 22:37:28 +0000

    Added css styling to dashboard

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,21 +1,3 @@
-// var searchInput = $('#search-input');
-// var searchButton = $('#search-button');
-// var weatherToday = $('#today');
-// var weatherForecast = $('#forecast');
-// var searchHistory = $('#history');
-// var searchForm = $('#search-form');
-
-// obtain today's date from momentjs
-var todaysDate = moment().format('DD/MM/YYYY');
-// console.log('the date today: ' + todaysDate);
-
-var theDate = moment().add(1, 'days').format('DD/MM/YYYY');
-// console.log('in three days: ' + theDate);
-
-// var newDate = moment(JSON.stringify(results.list[index].dt_txt)).format('DD/MM/YYYY');
-// console.log('this is the formatted date:' + newDate);
-
-
 $('#search-button').on('click', function(event) {
     event.preventDefault();
 
@@ -39,6 +21,10 @@ function searchHistory() {
     var theCity = cityName.charAt(0).toUpperCase() + cityName.slice(1);
     searchHistory.unshift(theCity);
 
+    // if (searchHistory.includes(theCity)) {
+    //     return;
+    // }
+
     // add city name data tags to cities
     button.attr("data-name", theCity);
     button.addClass("list-group-item btn city-button");
@@ -59,6 +45,7 @@ function searchHistory() {
         
     }
 }
+
 
 function buildQueryURL() {
     var cityName = $('#search-input').val().trim();
@@ -87,17 +74,22 @@ function buildQueryURL() {
             var temp =  JSON.stringify(results.list[0].main.temp) + "°C";
             var wind =  JSON.stringify(results.list[3].wind.speed) + " KPH";
             var humidity = JSON.stringify(results.list[0].main.humidity) + "%";
-            var weatherIcon = results.list[1].weather[0].icon;
+            var weatherIcon = 'https://openweathermap.org/img/wn/' + (results.list[1].weather[0].icon) + '.png';
 
-            $('#today').append(('<h2>') + theCity + " " + "(" + dayDate + ")" + " " + weatherIcon + ('</h2>'));
+            var icon = $('<img>')
+            .addClass('weather-icon')
+            .attr('src', weatherIcon);
 
+            $('#today').append(('<h2>') + theCity + " " + "(" + dayDate + ")" + ('</h2>'));
+            $('#today').append(icon);
+            
             // display current weather data to page
             $('#today').append(($('<p>').addClass('weather-metrics')).text('Temp: ' + temp));
             $('#today').append(($('<p>').addClass('weather-metrics')).text('Wind: ' + wind));
             $('#today').append(($('<p>').addClass('weather-metrics')).text('Humidity: ' + humidity));
 
             // display future weather data to page
-            $('#today').append($('<h3>').text("5-Day Forecast:"));
+            $('#forecast').prepend($('<h3>').text("5-Day Forecast:"));
         })
     })
 }
@@ -127,45 +119,47 @@ function getForecast() {
             method: "GET"
         }).then(function(results){
 
-            $('#display-forecast-1').append(($('<li>').addClass('list-group-item')).text(moment(results.list[6].dt_txt).format('DD/MM/YYYY')));
-            $('#display-forecast-1').append(($('<li>').addClass('list-group-item')).text(results.list[6].weather[0].icon));
-            $('#display-forecast-1').append(($('<li>').addClass('list-group-item')).text(JSON.stringify(results.list[6].main.temp) + "°C"));
-            $('#display-forecast-1').append(($('<li>').addClass('list-group-item')).text(JSON.stringify(results.list[6].wind.speed) + " KPH"));
-            $('#display-forecast-1').append(($('<li>').addClass('list-group-item')).text(JSON.stringify(results.list[6].main.humidity) + "%"));
+            $('#display-forecast-1').append(($('<p>').addClass('forecast-metric forecast-date')).text(moment(results.list[6].dt_txt).format('DD/MM/YYYY')));
+            $('#display-forecast-1').append(($('<img>').addClass('forecast-metric icon')).attr('src', 'https://openweathermap.org/img/wn/' + (results.list[6].weather[0].icon) + '.png'));
+            $('#display-forecast-1').append(($('<p>').addClass('forecast-metric')).text("Temp: " + JSON.stringify(results.list[6].main.temp) + "°C"));
+            $('#display-forecast-1').append(($('<p>').addClass('forecast-metric')).text("Wind: " + JSON.stringify(results.list[6].wind.speed) + " KPH"));
+            $('#display-forecast-1').append(($('<p>').addClass('forecast-metric')).text("Humidity: " + JSON.stringify(results.list[6].main.humidity) + "%"));
 
-            $('#display-forecast-2').append(($('<li>').addClass('list-group-item')).text(moment(results.list[14].dt_txt).format('DD/MM/YYYY')));
-            $('#display-forecast-2').append(($('<li>').addClass('list-group-item')).text(results.list[14].weather[0].icon));
-            $('#display-forecast-2').append(($('<li>').addClass('list-group-item')).text(JSON.stringify(results.list[14].main.temp) + "°C"));
-            $('#display-forecast-2').append(($('<li>').addClass('list-group-item')).text(JSON.stringify(results.list[14].wind.speed) + " KPH"));
-            $('#display-forecast-2').append(($('<li>').addClass('list-group-item')).text(JSON.stringify(results.list[14].main.humidity) + "%"));
+            $('#display-forecast-2').append(($('<p>').addClass('forecast-metric forecast-date')).text(moment(results.list[14].dt_txt).format('DD/MM/YYYY')));
+            $('#display-forecast-2').append(($('<img>').addClass('forecast-metric icon')).attr('src', 'https://openweathermap.org/img/wn/' + (results.list[14].weather[0].icon) + '.png'));
+            $('#display-forecast-2').append(($('<p>').addClass('forecast-metric')).text("Temp: " + JSON.stringify(results.list[14].main.temp) + "°C"));
+            $('#display-forecast-2').append(($('<p>').addClass('forecast-metric')).text("Wind: " + JSON.stringify(results.list[14].wind.speed) + " KPH"));
+            $('#display-forecast-2').append(($('<p>').addClass('forecast-metric')).text("Humidity: " + JSON.stringify(results.list[14].main.humidity) + "%"));
 
-            $('#display-forecast-3').append(($('<li>').addClass('list-group-item')).text(moment(results.list[22].dt_txt).format('DD/MM/YYYY')));
-            $('#display-forecast-3').append(($('<li>').addClass('list-group-item')).text(results.list[22].weather[0].icon));
-            $('#display-forecast-3').append(($('<li>').addClass('list-group-item')).text(JSON.stringify(results.list[22].main.temp) + "°C"));
-            $('#display-forecast-3').append(($('<li>').addClass('list-group-item')).text(JSON.stringify(results.list[22].wind.speed) + " KPH"));
-            $('#display-forecast-3').append(($('<li>').addClass('list-group-item')).text(JSON.stringify(results.list[22].main.humidity) + "%"));
+            $('#display-forecast-3').append(($('<p>').addClass('forecast-metric forecast-date')).text(moment(results.list[22].dt_txt).format('DD/MM/YYYY')));
+            $('#display-forecast-3').append(($('<img>').addClass('forecast-metric icon')).attr('src', 'https://openweathermap.org/img/wn/' + (results.list[22].weather[0].icon) + '.png'));
+            $('#display-forecast-3').append(($('<p>').addClass('forecast-metric')).text("Temp: " + JSON.stringify(results.list[22].main.temp) + "°C"));
+            $('#display-forecast-3').append(($('<p>').addClass('forecast-metric')).text("Wind: " + JSON.stringify(results.list[22].wind.speed) + " KPH"));
+            $('#display-forecast-3').append(($('<p>').addClass('forecast-metric')).text("Humidity: " + JSON.stringify(results.list[22].main.humidity) + "%"));
 
-            $('#display-forecast-4').append(($('<li>').addClass('list-group-item')).text(moment(results.list[30].dt_txt).format('DD/MM/YYYY')));
-            $('#display-forecast-4').append(($('<li>').addClass('list-group-item')).text(results.list[30].weather[0].icon));
-            $('#display-forecast-4').append(($('<li>').addClass('list-group-item')).text(JSON.stringify(results.list[30].main.temp) + "°C"));
-            $('#display-forecast-4').append(($('<li>').addClass('list-group-item')).text(JSON.stringify(results.list[30].wind.speed) + " KPH"));
-            $('#display-forecast-4').append(($('<li>').addClass('list-group-item')).text(JSON.stringify(results.list[30].main.humidity) + "%"));
+            $('#display-forecast-4').append(($('<p>').addClass('forecast-metric forecast-date')).text(moment(results.list[30].dt_txt).format('DD/MM/YYYY')));
+            $('#display-forecast-4').append(($('<img>').addClass('forecast-metric icon')).attr('src', 'https://openweathermap.org/img/wn/' + (results.list[30].weather[0].icon) + '.png'));
+            $('#display-forecast-4').append(($('<p>').addClass('forecast-metric')).text("Temp: " + JSON.stringify(results.list[30].main.temp) + "°C"));
+            $('#display-forecast-4').append(($('<p>').addClass('forecast-metric')).text("Wind: " + JSON.stringify(results.list[30].wind.speed) + " KPH"));
+            $('#display-forecast-4').append(($('<p>').addClass('forecast-metric')).text("Humidity: " + JSON.stringify(results.list[30].main.humidity) + "%"));
 
-            $('#display-forecast-5').append(($('<li>').addClass('list-group-item')).text(moment(results.list[38].dt_txt).format('DD/MM/YYYY')));
-            $('#display-forecast-5').append(($('<li>').addClass('list-group-item')).text(results.list[38].weather[0].icon));
-            $('#display-forecast-5').append(($('<li>').addClass('list-group-item')).text(JSON.stringify(results.list[38].main.temp) + "°C"));
-            $('#display-forecast-5').append(($('<li>').addClass('list-group-item')).text(JSON.stringify(results.list[38].wind.speed) + " KPH"));
-            $('#display-forecast-5').append(($('<li>').addClass('list-group-item')).text(JSON.stringify(results.list[38].main.humidity) + "%"));
+            $('#display-forecast-5').append(($('<p>').addClass('forecast-metric forecast-date')).text(moment(results.list[38].dt_txt).format('DD/MM/YYYY')));
+            $('#display-forecast-5').append(($('<img>').addClass('forecast-metric icon')).attr('src', 'https://openweathermap.org/img/wn/' + (results.list[38].weather[0].icon) + '.png'));
+            $('#display-forecast-5').append(($('<p>').addClass('forecast-metric')).text("Temp: " + JSON.stringify(results.list[38].main.temp) + "°C"));
+            $('#display-forecast-5').append(($('<p>').addClass('forecast-metric')).text("Wind: " + JSON.stringify(results.list[38].wind.speed) + " KPH"));
+            $('#display-forecast-5').append(($('<p>').addClass('forecast-metric')).text("Humidity: " + JSON.stringify(results.list[38].main.humidity) + "%"));
 
         })
 
     })
 }
 
+
 function clearForm() {
     $('#today').empty();
     $('.weather-forecast').empty();
 }
+
 
 function displayHistory() {
     var button = $('<button>');
